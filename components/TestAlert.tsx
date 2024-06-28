0e15a3ca2fcbe3115a4419695d055827d60be04a
Lzyoung <lzyoung666@gmail.com>
2024-06-28 17:35:35 +0200

    feat: Update TestStatus enum value to "pass" in scanResponse.ts
    feat: Add www_https module back with updated description in module.ts
    
    feat: Update TestAlert component to use "pass" instead of "passed" in TestAlert.tsx
    
    feat: Add case for "www_https" in switch statement in Content.tsx

--- components/TestAlert.tsx
+++ components/TestAlert.tsx
@@ -9,7 +9,7 @@ const TestAlert: React.FC<TestAlertProps> = ({ result }) => {
   const colorVariants = {
     fail: "bg-gray-100 border-gray-500 text-gray-700",
     warning: "bg-yellow-100 border-yellow-500 text-yellow-700",
-    passed: "bg-green-100 border-green-500 text-green-700",
+    pass: "bg-green-100 border-green-500 text-green-700",
   };
 
   return (
